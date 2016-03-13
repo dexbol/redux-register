66628a611f515429c7bc457724f721f75c16754e
dexbol <dexbolg@gmail.com>
2016-03-13 16:50:21 +0800

    Add registerByMap

--- src/index.js
+++ src/index.js
@@ -47,13 +47,82 @@ var rootReducer = function(state, action) {
     return finalState;
 };
 
+var checkTypeNamespace = function(ns, action) {
+    // The action type does not include a namespace or a right namespace.
+    return action.type.indexOf('.') < 0 || action.type.indexOf(ns + '.') === 0;
+};
+
+var enhanceStore = function(store) {
+    store.register = function(namespace, reducer) {
+        registerReducer(...arguments);
+        this.replaceReducer(rootReducer);
+    };
+
+    store.registerByMap = function(namespace, initialState, mapobj) {
+        if (process.env.NODE_ENV != 'production' && mapobj) {
+            for (var p in mapobj) {
+                if (p === 'undefined') {
+                    console.error('ReducerMap object has a undefined key. ' +
+                        'namespace=' + namespace);
+                } else if (p.indexOf('.') < 0) {
+                    console.warn('You maybe need a action type that includes '+
+                        'namespace <'
+                        + p + '>. The action type includes namespace can ' +
+                        'improve performance.');
+                }
+            }
+        }
+
+        var mapobj = mapobj || {};
+        var updateType = namespace + '.UPDATE';
+        if (!mapobj[updateType]) {
+            mapobj[updateType] = function(state, action) {
+                if (typeof state.merge == 'function') {
+                    return state.merge(action.payload);
+                }
+                return state;
+            };
+        }
+
+        var initType = namespace + '.INIT';
+        if (!mapobj[initType]) {
+            mapobj[initType] = function(state, action) {
+                return initialState;
+            }
+        }
+
+        this.register(namespace, function(state, action) {
+            if (!state) {
+                return initialState
+            }
+
+            if (!checkTypeNamespace(namespace, action)) {
+                return state;
+            }
+
+            if (process.env.NODE_ENV != 'production' &&
+                action.type.indexOf('@@redux') < 0) {
+                if (typeof mapobj[action.type] !== 'function') {
+                    console.error('The action type "' + action.type +
+                        '" does not define yet.');
+                } else if (mapobj[action.type](state, action) === undefined) {
+                    console.error('The reducer should return a new ' +
+                        'state. ' + action.type + 'return undefined');
+                }
+            }
+
+            return typeof mapobj[action.type] === 'function' ?
+                mapobj[action.type](state, action) : state;
+        });
+    };
+
+    return store;
+};
+
 export default function Register() {
     return (next) => (reducer, initialState) => {
-        var store = next(rootReducer, initialState);
-        store.registerReducer = function(namespace, reducer) {
-            registerReducer(... arguments);
-            this.replaceReducer(rootReducer);
-        };
+        var store = enhanceStore(next(rootReducer, initialState));
+
         return store;
     };
 };
